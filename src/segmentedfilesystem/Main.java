f87184fb0dee72e6a44e3883c40a0a919572ace5
corde171 <corde171@morris.umn.edu>
2019-12-03 11:06:10 -0600

    Handle adding packet to FilePackets

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,20 +1,21 @@
 package segmentedfilesystem;
 
-import java.util.ArrayList;
+import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Main {
     private final int portNum = 6014;
     private final String server = "csci-4409.morris.umn.edu";
     public static void main(String[] args) {
-        
+
     }
 
     static class DataPacket {
         byte statusByte;
         byte fileId;
         byte[] packetNumber = new byte[2];
-        byte[] data = new byte[1024];
+        byte[] data;
         boolean header = false;
         boolean isLast = false;
         public DataPacket(byte[] bytes) {
@@ -27,25 +28,36 @@ public class Main {
                 this.header = true;
             }
             if(this.header) {
-                System.arraycopy(bytes, 2, this.data,0,6);
+                this.data = Arrays.copyOfRange(bytes, 2, bytes.length);
             }
             else {
                 this.packetNumber[0] = bytes[2];
                 this.packetNumber[1] = bytes[3];
-                System.arraycopy(bytes, 4, this.data,0,6);
+                this.data = Arrays.copyOfRange(bytes, 4, bytes.length);
             }
         }
     }
 
     static class FilePackets {
-        private ArrayList<DataPacket> packets;
         private DataPacket header;
         private HashMap<Integer, DataPacket> map;
+        private int last;
         public FilePackets(DataPacket header) {
             this.header = header;
         }
         public void addPacket(DataPacket packet) {
-            this.packets.add(packet);
+            if(packet.header){
+                header = packet;
+            }else{
+                int num = new BigInteger(packet.packetNumber).intValue();
+                if(packet.isLast){
+                    last = num;
+                }
+                map.put(num, packet);
+            }
+            if(map.size() == last+1){
+                //we're done
+            }
         }
     }
 
--- test/segmentedfilesystem/ClientTest.java
+++ test/segmentedfilesystem/ClientTest.java
@@ -21,12 +21,24 @@ public class ClientTest {
     }
 
     @Test
-    public void dataPacketDataTest() {
+    public void dataPacketLastTest() {
         byte[] bytes = new byte[1024];
         byte[] temp = {15,0,55,110,67,68,59};
         System.arraycopy(temp, 0, bytes,0,6);
         Main.DataPacket packet = new Main.DataPacket(bytes);
         assertFalse(packet.header);
+        assertTrue(packet.isLast);
     }
+    @Test
+    public void dataPacketDataTest() {
+        byte[] bytes = new byte[1024];
+        byte[] temp = {13,0,55,110,67,68,59};
+        System.arraycopy(temp, 0, bytes,0,6);
+        Main.DataPacket packet = new Main.DataPacket(bytes);
+        assertFalse(packet.header);
+        assertFalse(packet.isLast);
+    }
+
+
 
 }
