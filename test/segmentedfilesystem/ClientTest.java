1bf5f15fd61ffde2eb8ca6d1a6d4dfae744b6b44
corde171 <corde171@morris.umn.edu>
2019-12-03 10:49:00 -0600

    Add some tests

---
+++ test/segmentedfilesystem/ClientTest.java
@@ -0,0 +1,32 @@
+package segmentedfilesystem;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * This is just a stub test file. You should rename it to
+ * something meaningful in your context and populate it with
+ * useful tests.
+ */
+public class ClientTest {
+
+    @Test
+    public void dataPacketHeaderTest() {
+        byte[] bytes = new byte[1024];
+        byte[] temp = {16,0,55,110,67,68,59};
+        System.arraycopy(temp, 0, bytes,0,6);
+        Main.DataPacket packet = new Main.DataPacket(bytes);
+        assertTrue(packet.header);
+    }
+
+    @Test
+    public void dataPacketDataTest() {
+        byte[] bytes = new byte[1024];
+        byte[] temp = {15,0,55,110,67,68,59};
+        System.arraycopy(temp, 0, bytes,0,6);
+        Main.DataPacket packet = new Main.DataPacket(bytes);
+        assertFalse(packet.header);
+    }
+
+}
