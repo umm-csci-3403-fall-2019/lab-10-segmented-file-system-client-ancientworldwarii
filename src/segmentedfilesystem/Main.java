5541eb299f75aba5fce605a21d15137dd8e64594
corde171 <corde171@morris.umn.edu>
2019-12-03 09:57:25 -0600

    Implement basic DataPacket class

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -6,4 +6,27 @@ public class Main {
         
     }
 
+    class DataPacket {
+        byte statusByte;
+        byte fileId;
+        byte[] packetNumber = new byte[2];
+        byte[] data = new byte[1024];
+        boolean header = false;
+        public DataPacket(byte[] bytes) {
+            this.statusByte = bytes[0];
+            this.fileId = bytes[1];
+            if(this.statusByte % 2 == 0) {
+                this.header = true;
+            }
+            if(this.header) {
+                System.arraycopy(bytes, 2, this.data,0,1024);
+            }
+            else {
+                this.packetNumber[0] = bytes[2];
+                this.packetNumber[1] = bytes[3];
+                System.arraycopy(bytes, 4, this.data,0,1024);
+            }
+        }
+    }
+
 }
