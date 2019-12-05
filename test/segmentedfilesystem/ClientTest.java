11382f02180da22bad5fbc918dcdaf9410374b17
corde171 <corde171@morris.umn.edu>
2019-12-05 10:29:31 -0600

    Add new tests to ClientTest.java including ones for FilePackets

--- test/segmentedfilesystem/ClientTest.java
+++ test/segmentedfilesystem/ClientTest.java
@@ -15,7 +15,7 @@ public class ClientTest {
     public void dataPacketHeaderTest() {
         byte[] bytes = new byte[1024];
         byte[] temp = {16,0,55,110,67,68,59};
-        System.arraycopy(temp, 0, bytes,0,6);
+        System.arraycopy(temp, 0, bytes,0,7);
         Main.DataPacket packet = new Main.DataPacket(bytes);
         assertTrue(packet.header);
     }
@@ -24,7 +24,7 @@ public class ClientTest {
     public void dataPacketLastTest() {
         byte[] bytes = new byte[1024];
         byte[] temp = {15,0,55,110,67,68,59};
-        System.arraycopy(temp, 0, bytes,0,6);
+        System.arraycopy(temp, 0, bytes,0,7);
         Main.DataPacket packet = new Main.DataPacket(bytes);
         assertFalse(packet.header);
         assertTrue(packet.isLast);
@@ -33,12 +33,25 @@ public class ClientTest {
     public void dataPacketDataTest() {
         byte[] bytes = new byte[1024];
         byte[] temp = {13,0,55,110,67,68,59};
-        System.arraycopy(temp, 0, bytes,0,6);
+        System.arraycopy(temp, 0, bytes,0,7);
         Main.DataPacket packet = new Main.DataPacket(bytes);
         assertFalse(packet.header);
         assertFalse(packet.isLast);
     }
 
-
+    @Test
+    public void dataPacketsFullTest() {
+        byte[] bytes = new byte[1024];
+        byte[] temp = {3,0,0,0,67,68,59};
+        System.arraycopy(temp, 0,bytes,0,7);
+        Main.DataPacket packet = new Main.DataPacket(bytes);
+        bytes[0] = 0;
+        Main.FilePackets file = new Main.FilePackets(new Main.DataPacket(bytes));
+        assertFalse(file.isDone());
+        file.addPacket(packet);
+        assertTrue(file.isDone());
+        file.addPacket(new Main.DataPacket(bytes));
+        assertTrue(file.isDone());
+    }
 
 }
